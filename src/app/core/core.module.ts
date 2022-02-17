6f51aef73ca3fc6aa8b86fa50a69981beae4f535
szczesniakm <michal.szczesniak.stud@pw.edu.pl>
2022-02-18 00:03:45 +0100

    Add footer template.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   exports: [
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
